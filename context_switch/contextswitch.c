f4a9709eaab12eafd98ba8c2848e4d659c71a020
ShreyasiKothawade <kothawadeshreyasi2684@gmail.com>
2025-03-24 13:20:37 +0530

    pushed all to main

--- context_switch/contextswitch.c
+++ context_switch/contextswitch.c
@@ -13,43 +13,35 @@
 ucontext_t contexts[MAX_TASKS];
 ucontext_t main_context;
 int current_task = 0;
-int task_time_elapsed = 0;
 
 void task1() {
     while (1) {
+        printf("Task 1 is running...\n");
         sleep(1);
     }
 }
 
 void task2() {
     while (1) {
+        printf("Task 2 is running...\n");
         sleep(1);
     }
 }
 
 void signal_handler(int sig) {
-    task_time_elapsed++;
-    printf("Task %d is running... %d seconds elapsed\n", current_task + 1, task_time_elapsed);
-    if (task_time_elapsed >= TASK_DURATION) {
-        printf("Task %d completed %d seconds. Switching tasks...\n", current_task + 1, TASK_DURATION);
-        int prev_task = current_task;
-        current_task = (current_task + 1) % MAX_TASKS;
-        task_time_elapsed = 0;
-        swapcontext(&contexts[prev_task], &contexts[current_task]);
-    }
+    int prev_task = current_task;
+    current_task = (current_task + 1) % MAX_TASKS;
+    printf("Switching to Task %d...\n", current_task + 1);
+    swapcontext(&contexts[prev_task], &contexts[current_task]);
 }
 
-void setup_timer(int use_virtual) {
+void setup_timer() {
     struct itimerval timer;
-    timer.it_value.tv_sec = 1;
+    timer.it_value.tv_sec = TASK_DURATION;   
     timer.it_value.tv_usec = 0;
-    timer.it_interval.tv_sec = 1;
+    timer.it_interval.tv_sec = TASK_DURATION;  
     timer.it_interval.tv_usec = 0;
-    if (use_virtual) {
-        setitimer(ITIMER_VIRTUAL, &timer, NULL);
-    } else {
-        setitimer(ITIMER_REAL, &timer, NULL);
-    }
+    setitimer(ITIMER_REAL, &timer, NULL);
 }
 
 void create_task(void (*func)(), ucontext_t *context) {
@@ -61,28 +53,20 @@ void create_task(void (*func)(), ucontext_t *context) {
     makecontext(context, func, 0);
 }
 
-void setup_signals(int use_virtual) {
+void setup_signals() {
     struct sigaction sa;
     sa.sa_handler = signal_handler;
     sa.sa_flags = SA_RESTART;
     sigemptyset(&sa.sa_mask);
-    if (use_virtual) {
-        sigaction(SIGVTALRM, &sa, NULL);
-    } else {
-        sigaction(SIGALRM, &sa, NULL);
-    }
+    sigaction(SIGALRM, &sa, NULL);
 }
 
-int main(int argc, char *argv[]) {
-    int use_virtual = 0;
-    if (argc > 1 && atoi(argv[1]) == 1) {
-        use_virtual = 1;
-    }
+int main() {
     printf("Initializing tasks...\n");
     create_task(task1, &contexts[0]);
     create_task(task2, &contexts[1]);
-    setup_signals(use_virtual);
-    setup_timer(use_virtual);
+    setup_signals();
+    setup_timer();
     printf("Starting Task 1...\n");
     swapcontext(&main_context, &contexts[0]);
     printf("Back to main. All tasks completed!\n");
